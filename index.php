075a1f82c0fc913fdba0c9b8c0811c62e76bb4fc
Luis rojas <luis.alberto.rojas@bootcamp-upgrade.com>
2024-05-13 01:33:17 +0200

    Primer commit ejercicio php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// Declaramos las variables
+$texto = "En algun lugar de la mancha"; 
+$autor = "Miguel de cervante";
+$user = "ElmancoDeLepanto";
+
+// Declaro el require
+require_once "index.html";
