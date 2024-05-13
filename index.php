5499149a2337eaee77f3b22729647f427373945f
Luis rojas <luis.alberto.rojas@bootcamp-upgrade.com>
2024-05-13 10:34:05 +0200

    muestro un listado de tweets

--- index.php
+++ index.php
@@ -5,5 +5,24 @@ $texto = "En algun lugar de la mancha";
 $autor = "Miguel de cervante";
 $user = "ElmancoDeLepanto";
 
+//hacemos una variable con un array para generar varios tweets
+$tweets = [
+    [
+      "texto"=>"En algun lugar de la mancha",
+      "autor"=>"Miguel de Cervantes",
+      "user"=>"EnMancoDeLepanto"
+    ],
+    [
+      "texto"=>"El señor de los anillos",
+      "autor"=>"Tolkien",
+      "user"=>"Martha"
+    ],
+    [
+      "texto"=>"La llamada de cthulhu",
+      "autor"=>"LOvecraft",
+      "user"=>"Lrojs",
+    ],
+];
+
 // Declaro el require
 require_once "index.html";
